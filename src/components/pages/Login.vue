778ede8910ccca73b2b4a62aad8c83846a9ebf5c
colinchiu <colinchiu87@gmail.com>
2021-09-07 16:22:16 +0800

    [amend] 調整登錄頁樣式

--- src/components/pages/Login.vue
+++ src/components/pages/Login.vue
@@ -6,38 +6,37 @@
         src="https://getbootstrap.com/docs/5.0/assets/brand/bootstrap-logo.svg"
         alt=""
         width="72"
-        height="57"
+        height="72"
       />
-      <h1 class="h3 mb-3 fw-normal">請先登入</h1>
-      <div class="form-floating">
-        <input
-          type="email"
-          class="form-control"
-          id="floatingInput"
-          placeholder="name@example.com"
-          v-model="user.username"
-        />
-        <label for="floatingInput">Email address</label>
-      </div>
-      <div class="form-floating">
-        <input
-          type="password"
-          class="form-control"
-          id="floatingPassword"
-          placeholder="Password"
-          v-model="user.password"
-        />
-        <label for="floatingPassword">Password</label>
-      </div>
+      <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+      <label for="inputEmail" class="sr-only">Email address</label>
+      <input
+        type="email"
+        id="inputEmail"
+        class="form-control"
+        placeholder="Email address"
+        required
+        autofocus
+        v-model="user.username"
+      />
+      <label for="inputPassword" class="sr-only">Password</label>
+      <input
+        type="password"
+        id="inputPassword"
+        class="form-control"
+        placeholder="Password"
+        required
+        v-model="user.password"
+      />
       <div class="checkbox mb-3">
         <label>
           <input type="checkbox" value="remember-me" /> Remember me
         </label>
       </div>
-      <button class="w-100 btn btn-lg btn-primary" type="submit">
+      <button class="btn btn-lg btn-primary btn-block" type="submit">
         Sign in
       </button>
-      <p class="mt-5 mb-3 text-muted">&copy; 2017–2021</p>
+      <p class="mt-5 mb-3 text-muted">&copy; 2017-2021</p>
     </form>
   </div>
 </template>
@@ -72,47 +71,45 @@ export default {
   },
 };
 </script>
-<sytle lang="scss" scoped>
+<style lang="scss" scoped>
 .login {
-  text-align: center !important;
-  // display: flex;
-  // align-items: center;
-  // justify-content: center;
-}
-body {
-  height: 100%;
-}
-
-body {
-  // padding-top: 40px;
-  // padding-bottom: 40px;
+  height: 100vh;
+  display: -ms-flexbox;
+  display: flex;
+  -ms-flex-align: center;
+  align-items: center;
+  padding-top: 40px;
+  padding-bottom: 40px;
   background-color: #f5f5f5;
+  text-align: center !important;
 }
-
 .form-signin {
   width: 100%;
   max-width: 330px;
   padding: 15px;
   margin: auto;
 }
-
 .form-signin .checkbox {
   font-weight: 400;
 }
-
-.form-signin .form-floating:focus-within {
+.form-signin .form-control {
+  position: relative;
+  box-sizing: border-box;
+  height: auto;
+  padding: 10px;
+  font-size: 16px;
+}
+.form-signin .form-control:focus {
   z-index: 2;
 }
-
 .form-signin input[type="email"] {
   margin-bottom: -1px;
   border-bottom-right-radius: 0;
   border-bottom-left-radius: 0;
 }
-
 .form-signin input[type="password"] {
   margin-bottom: 10px;
   border-top-left-radius: 0;
   border-top-right-radius: 0;
 }
-</sytle>
+</style>

d58e8ee8b512b285e420c7f387af9dcb0c80c35d
colinchiu <colinchiu87@gmail.com>
2021-09-08 17:18:43 +0800

    [feature] 完成修改product功能

--- src/components/pages/Products.vue
+++ src/components/pages/Products.vue
@@ -277,7 +277,7 @@ export default {
         this.tempProduct = {};
         this.isNew = true;
       } else {
-        this.tempProduct = Object.assign({}.item);
+        this.tempProduct = Object.assign({}, item);
         this.isNew = false;
       }
       $("#productModal").modal("show");
